dde6877e395a4c085c90bf742cbdcef45ba85994
Shariq Ansari <mohammed.shariq@iris-worldwide.com>
2016-05-19 18:25:51 +0530

    added video full width

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -3,6 +3,22 @@ $(document).ready(function(){
 	window.onload = function(){
 		$(window).stellar();
 	}
+
+	 var videos  = $(".video");
+ 
+        videos.on("click", function(){
+            var elm = $(this),
+                conts   = elm.contents(),
+                le      = conts.length,
+                ifr     = null;
+ 
+            for(var i = 0; i<le; i++){
+              if(conts[i].nodeType == 8) ifr = conts[i].textContent;
+            }
+ 
+            elm.addClass("player").html(ifr);
+            elm.off("click");
+        });
 	
 	$("html").niceScroll({horizrailenabled:false});
 	
